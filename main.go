22a7aba186877f1b3a4bab7716120ffaf6b8f575
mcnic <mn.maksim+github@gmail.com>
2025-10-08 01:16:28 -0700

    feat: added file path root

--- main.go
+++ main.go
@@ -7,14 +7,16 @@ import (
 
 func main(){
 	const port = "8080"
+	const filepathRoot = "."
 
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
 
 	srv := &http.Server{
 		Addr: ":" + port,
 		Handler: mux,
 	}
 
-	log.Printf("Serving on port :%s", port)
+	log.Printf("serving on port :%s : %s", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
 }
